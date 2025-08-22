12cd80d28e92100f3ad42631e3769c43c1afda30
Adam Bonny <agbonnyh@gmail.com>
2025-08-22 11:34:38 +0200

    Clean up
    Cleaned up some variable names

--- dist/ppal.es.js
+++ dist/ppal.es.js
@@ -1,30 +1,30 @@
-import c from "moment";
+import u from "moment";
 import r from "lodash";
 const n = [];
 for (let t = 0; t < 256; ++t)
   n.push((t + 256).toString(16).slice(1));
-function p(t, e = 0) {
+function S(t, e = 0) {
   return (n[t[e + 0]] + n[t[e + 1]] + n[t[e + 2]] + n[t[e + 3]] + "-" + n[t[e + 4]] + n[t[e + 5]] + "-" + n[t[e + 6]] + n[t[e + 7]] + "-" + n[t[e + 8]] + n[t[e + 9]] + "-" + n[t[e + 10]] + n[t[e + 11]] + n[t[e + 12]] + n[t[e + 13]] + n[t[e + 14]] + n[t[e + 15]]).toLowerCase();
 }
-let d;
-const S = new Uint8Array(16);
-function g() {
-  if (!d) {
+let a;
+const g = new Uint8Array(16);
+function l() {
+  if (!a) {
     if (typeof crypto > "u" || !crypto.getRandomValues)
       throw new Error("crypto.getRandomValues() not supported. See https://github.com/uuidjs/uuid#getrandomvalues-not-supported");
-    d = crypto.getRandomValues.bind(crypto);
+    a = crypto.getRandomValues.bind(crypto);
   }
-  return d(S);
+  return a(g);
 }
 const h = typeof crypto < "u" && crypto.randomUUID && crypto.randomUUID.bind(crypto), m = { randomUUID: h };
 function y(t, e, o) {
   if (m.randomUUID && !t)
     return m.randomUUID();
   t = t || {};
-  const i = t.random ?? t.rng?.() ?? g();
+  const i = t.random ?? t.rng?.() ?? l();
   if (i.length < 16)
     throw new Error("Random bytes length must be >= 16");
-  return i[6] = i[6] & 15 | 64, i[8] = i[8] & 63 | 128, p(i);
+  return i[6] = i[6] & 15 | 64, i[8] = i[8] & 63 | 128, S(i);
 }
 const x = {
   /**
@@ -44,14 +44,14 @@ const x = {
    * Returns the default date
    * @returns { string } The current date and time formatted as an ISO string.
    */
-  date: () => c(/* @__PURE__ */ new Date()).toISOString()
+  date: () => u(/* @__PURE__ */ new Date()).toISOString()
 }, w = {
   /**
    * Generates a unique MongoDB-compatible ObjectId
    * @returns {string} A new MongoDB ObjectId string.
    */
   generateObjectId: () => {
-    const t = ((/* @__PURE__ */ new Date()).getTime() / 1e3 | 0).toString(16), e = "xxxxxxxxxxxxxxxx".replace(/[x]/g, () => (Math.random() * 16 | 0).toString(16)).toLowerCase();
+    const t = ((/* @__PURE__ */ new Date()).getTime() / 1e3 | 0).toString(16), e = "xxxxxxxxxxxxxxxx".replace(/x/g, () => (Math.random() * 16 | 0).toString(16)).toLowerCase();
     return `${t}${e}`;
   },
   /**
@@ -62,31 +62,31 @@ const x = {
   // ALIASES
   wrap: x.id
   // ObjectId BSON wrapper
-}, f = {
+}, D = {
   /**
    * Compares two objects and returns an object containing only the differences
    * @param { Object } inputs
    * @param { Object } inputs.original Required: The value of this object are considered the default in this comparison.
    * @param { Object } inputs.updated Required: The value of this object will be returned as output if they're different from inputs.original.
    * @param { string } inputs.prefix Optional: Default is an empty string.
-   * @returns { Object } An object containing only the differences between inputs.original and inputs.updated.
+   * @returns { Object } An object containing only the differences between `inputs.original` and `inputs.updated`.
    */
   diff: ({ original: t, updated: e, prefix: o = "" }) => !r.isObject(t) || !r.isObject(e) ? r.isEqual(t, e) ? {} : { [o.slice(0, -1)]: e } : r.isArray(t) && r.isArray(e) ? r.isEqual(t, e) ? {} : { [o.slice(0, -1)]: e } : r.transform(
     e,
-    (i, s, u) => {
-      const a = o + u;
-      if (!r.has(t, u))
-        i[a] = s;
-      else if (!r.isEqual(s, t[u]))
-        if (r.isObject(s) && r.isObject(t[u])) {
-          const l = (void 0).diff({
-            original: t[u],
+    (i, s, c) => {
+      const d = o + c;
+      if (!r.has(t, c))
+        i[d] = s;
+      else if (!r.isEqual(s, t[c]))
+        if (r.isObject(s) && r.isObject(t[c])) {
+          const p = (void 0).diff({
+            original: t[c],
             value: s,
-            prefix: `${a}`
+            prefix: `${d}`
           });
-          r.assign(i, l);
+          r.assign(i, p);
         } else
-          i[a] = s;
+          i[d] = s;
     },
     {}
   ),
@@ -98,7 +98,7 @@ const x = {
    * @returns { boolean } Returns true if at least one property if found, returns false otherwise.
    */
   hasProperties: ({ object: t, properties: e }) => r.some(e, (o) => !r.isUndefined(r.get(t, o)))
-}, D = {
+}, I = {
   /**
    * Checks if a string is unique within an array of strings
    * @param { Object } inputs
@@ -107,7 +107,7 @@ const x = {
    * @returns { boolean } Returns true if the string isn't found in the array (meaning it would be unique if added to that array), returns false otherwise.
    */
   uniqueInArray: ({ string: t, array: e }) => !new Set(e).has(t)
-}, I = {
+}, f = {
   /**
    * Format a date in several standard formats
    * @param { Object } inputs
@@ -119,10 +119,10 @@ const x = {
   format: ({ date: t, type: e, timezone: o = "America/Denver" }) => {
     switch (e) {
       case "DayOnly":
-        let i = c(t).format("YYYY-MM-DD");
-        return c.tz(i, o).set({ hour: 6, minute: 0 }).toISOString();
+        let i = u(t).format("YYYY-MM-DD");
+        return u.tz(i, o).set({ hour: 6, minute: 0 }).toISOString();
       case "Timestamp":
-        return c(t).toISOString();
+        return u(t).toISOString();
       default:
         throw new Error(
           `${e}: This type isn't supported, please check your spelling.`
@@ -146,9 +146,9 @@ const x = {
   inputDiff: ({ value: t, initialData: e, theme: o }) => t !== e ? `${o.primary}40` : null
 }, j = {
   id: w,
-  object: f,
-  string: D,
-  date: I,
+  object: D,
+  string: I,
+  date: f,
   behavior: U
 };
 export {
